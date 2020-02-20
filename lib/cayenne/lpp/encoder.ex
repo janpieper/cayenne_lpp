cd15db4d68a48e7ee0d21670ce06090e5133a240
Jan Pieper <kontakt@jan-pieper.info>
2020-02-20 08:56:34 +0100

    Add note to encoder about adding new types

--- lib/cayenne/lpp/encoder.ex
+++ lib/cayenne/lpp/encoder.ex
@@ -2,6 +2,10 @@ defprotocol Cayenne.LPP.Encoder do
   @moduledoc """
   Cayenne LPP types need to implement this protocol to be encoded into
   a `Cayenne.LPP.Buffer`
+
+  **Note:** Adding new types does not mean they will be automagically
+  identified and/or supported by [TheThingsNetwork](https://thethingsnetwork.org/)
+  and/or [Cayenne Cloud](https://developers.mydevices.com/)!
   """
 
   @spec encode(t()) :: Cayenne.LPP.Buffer.t()
