4760bdd8c0db9052baae2250438a7daecf0d2693
Jan Pieper <kontakt@jan-pieper.info>
2020-02-20 17:54:35 +0100

    Prepare library to be published to hex.pm and hexdocs.pm

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Cayenne.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :cayenne_lpp,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() != :test,
       deps: deps(),
+      description: description(),
+      package: package(),
+      docs: docs(),
       test_coverage: [tool: ExCoveralls],
       prefered_cli_env: [
         coveralls: :test,
@@ -24,7 +30,52 @@ defmodule Cayenne.MixProject do
     [
       {:excoveralls, "~> 0.12.2", only: [:test]},
       {:credo, "~> 1.1.5", only: [:dev], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.21.3", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp description do
+    "cayenne_lpp is a library for working with Cayenne Low Power " <>
+      "Payload (LPP) for LoRaWAN applications"
+  end
+
+  defp package do
+    [
+      maintainers: ["Jan Pieper"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/janpieper/cayenne_lpp"},
+      files: ["lib", "test", "mix.exs", "README.md", "LICENSE.md", ".formatter.exs"]
+    ]
+  end
+
+  defp docs do
+    [
+      name: "cayenne_lpp",
+      source_ref: "v#{@version}",
+      canonical: "https://hexdocs.pm/cayenne_lpp",
+      source_url: "https://github.com/janpieper/cayenne_lpp",
+      extras: ["LICENSE.md"],
+      groups_for_modules: [
+        # Cayenne.LPP.Buffer
+        # Cayenne.LPP.Encoder
+        # Cayenne.LPP.Payload
+
+        Types: [
+          Cayenne.LPP.Type.Accelerometer,
+          Cayenne.LPP.Type.AnalogInput,
+          Cayenne.LPP.Type.AnalogOutput,
+          Cayenne.LPP.Type.BarometricPressure,
+          Cayenne.LPP.Type.DigitalInput,
+          Cayenne.LPP.Type.DigitalOutput,
+          Cayenne.LPP.Type.GPS,
+          Cayenne.LPP.Type.Gyrometer,
+          Cayenne.LPP.Type.Luminosity,
+          Cayenne.LPP.Type.Presence,
+          Cayenne.LPP.Type.RelativeHumidity,
+          Cayenne.LPP.Type.Temperature
+        ]
+      ]
     ]
   end
 end
